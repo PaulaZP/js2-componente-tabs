25fd98d0cafa0270766f735291968a6be609bd1a
Paula Zuñiga <mzp1405@gmail.com>
2020-09-15 17:45:41 -0600

    Terminar el javascript

--- js/script.js
+++ js/script.js
@@ -6,11 +6,15 @@ function clickHandler(event) {
     console.log(id);
 
     for(let i = 0; i < contentido.length; i++){
-        contentido[i].style.display = 'none';
+        contentido[i].classList.add('js-content-hidden');
         if (contentido[i].getAttribute('id') == id){
-            contentido[i].style.display = 'block';
+            contentido[i].classList.remove('js-content-hidden');
         }
     }
+    for(let i = 0; i < enlaces.length; i++){
+        enlaces[i].classList.remove('js-active');
+    }
+    event.currentTarget.classList.add('js-active')
 }
 
 const enlaces = document.querySelectorAll('.tabs-tablist-item');
@@ -18,10 +22,12 @@ const enlaces = document.querySelectorAll('.tabs-tablist-item');
 const contentido = document.querySelectorAll('.tabs-content-item');
 
 for(let i = 0; i < contentido.length; i++){
-    contentido[i].style.display = 'none';
+    contentido[i].classList.add('js-content-hidden');
 }
 
-contentido[0].style.display = 'block';
+contentido[0].classList.remove('js-content-hidden');
+
+enlaces[0].classList.remove('js-active');
 
 for(let i = 0; i < enlaces.length; i++){
     enlaces[i].addEventListener('click' , clickHandler);
